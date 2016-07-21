a2b3b87408f1c487b9c704bed5a4f8cddf35540d
root <root@f5b539b69030>
2016-07-21 03:17:29 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,11 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "defaultLanguage" : "en",
+  "displayName" : "STudioTest",
+  "homePage" : "Main",
+  "name" : "STudioTest",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
